de99f518d476e1ae447f5d5bbe353ce966394a1d
anu3-80 <anusha9392094@gmail.com>
2024-11-05 15:31:35 +0530

    modify

--- exception.java
+++ exception.java
@@ -2,9 +2,6 @@ class exception
 {
  public static void main(String[] args)
  {
-  System.out.println("before exception");
-  System.out.println(10/0);
-  System.out.println("after exception");
-  System.out.println("im executing");
+  	System.out.println("Hello");
  }
 }
