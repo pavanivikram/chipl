88e3242b815142714cc2b1e02e9f019911cf5696
pavanivikram <pavanivikram16@gmail.com>
2024-11-05 14:44:03 +0530

    java files

---
+++ exception.java
@@ -0,0 +1,10 @@
+class exception
+{
+ public static void main(String[] args)
+ {
+  System.out.println("before exception");
+  System.out.println(10/0);
+  System.out.println("after exception");
+  System.out.println("im executing");
+ }
+}
